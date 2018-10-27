199f628249f65fb6852241d75750f758e23cc05a
nspeer12 <nspeer252@gmail.com>
2018-10-27 18:17:12 -0400

    added new data and finished caloricIntake()

--- csvreader.py
+++ csvreader.py
@@ -15,7 +15,14 @@ def caloricIntake(sex, age):
 			return 2200
 
 	elif sex == "f":
-		return 1
+		if age >= 18 and age <= 25:
+			return 2200
+		if age > 25 and age <= 45:
+			return 2000
+		if age > 45 and age <= 65:
+			return 1800
+		if age > 65:
+			return 1600
 
 
 
@@ -26,17 +33,10 @@ def caloricIntake(sex, age):
 with open('data.csv', 'r') as csv_file:
 	csv_reader = csv.reader(csv_file)
 
-	#meals[3] # breakfast lunch and dinner
+	# obtain user information
 	age = input("What is your age in years? ")
-	#height = input("What is your height in cm ")
 	sex = input("what is your sex (m/f) ")
-	#Men	BMR = 66.4730 + (13.7516 x weight in kg) + (5.0033 x height in cm) – (6.7550 x age in years)
-	#Women BMR = 655.0955 + (9.5634 x weight in kg) + (1.8496 x height in cm) – (4.6756 x age in years)
 
-	#if sex == "m":
-	#	bmr = 66.473 + (13.7516 * float(weight)) + (5.0033 * float(height)) - (6.755 * float(age))
-	#elif sex == "f":
-	#	bmr = 655.0955 + (9.5634 * weight) + (1.8496 * height) - (4.6756 * age)
 	print("Your suggested caloric intake is " + str(caloricIntake(sex, age)))
 
 	#for line in csv_reader:
